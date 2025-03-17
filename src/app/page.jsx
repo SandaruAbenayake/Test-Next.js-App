3eacc12bb66d07fb963d9c9555d281284826b51e
SANDARU-IT\User <maggonageabenayake@gmail.com>
2025-03-17 13:45:58 +0530

    First Commit

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+    
+    <h1>Hello World</h1>
+    <h2>Hello World</h2>
+    </>
+    
+    
+  )
+}
